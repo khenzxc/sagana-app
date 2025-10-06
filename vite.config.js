11c4aae073b79fccb71736035b2acb541c477494
Khen Vonoe Gabriel <dalanginkhen@gmail.com>
2025-10-06 14:09:55 +0800

    Initial commit or update project files

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
   import { defineConfig } from 'vite'
   import react from '@vitejs/plugin-react'
 
-<<<<<<< HEAD
-// ✅ Gumagana sa localhost at GitHub Pages
-export default defineConfig({
-  plugins: [react()],
-})
-=======
   // https://vite.dev/config/
   export default defineConfig({
     plugins: [react()],
   })
->>>>>>> 7900f6e (fix: update vite config and reupload project)
