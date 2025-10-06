3df69951d4c9977acaadf9c8e9a74594f9ad3689
Khen Vonoe Gabriel <dalanginkhen@gmail.com>
2025-10-06 13:09:00 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // ✅ Gumagana sa localhost at GitHub Pages
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/sagana-app/' : '/', 
 })
