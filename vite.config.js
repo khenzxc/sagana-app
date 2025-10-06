ed94fd3eb3c801e7ad4026332d6bebb92a180c79
Khen Vonoe Gabriel <dalanginkhen@gmail.com>
2025-10-06 12:33:14 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/sagana-app/', // ← palitan ng eksaktong repo name mo
 })
